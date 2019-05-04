2c6f00ea93b110d94e6766e987f36aa4bb8bd23c
Tyler Johnston <tylergarrettjohnston@gmail.com>
2019-05-03 23:02:09 -0400

    Section 3, Lecture 50: Adding Content to the Recipes Components
    Section 3, Lecture 51: Outputting a List of Recipes with ngFor

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://media4.s-nbcnews.com/j/MSNBC/Components/Video/201802/' +
+        'tdy_pop_klg_steak_180215_1920x1080.today-inline-vid-featured-desktop.jpg'
+    )
+  ];
 
   constructor() { }
 
