0776d7cf879e9c45cbd11da83f38b4a8c25720a0
Tyler Johnston <tylergarrettjohnston@gmail.com>
2019-05-03 23:09:12 -0400

    Section 3, Lecture 52: Displaying Recipe Details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,8 +11,7 @@ export class RecipeListComponent implements OnInit {
     new Recipe(
       'A Test Recipe',
       'This is simply a test',
-      'https://media4.s-nbcnews.com/j/MSNBC/Components/Video/201802/' +
-        'tdy_pop_klg_steak_180215_1920x1080.today-inline-vid-featured-desktop.jpg'
+      'https://media4.s-nbcnews.com/j/MSNBC/Components/Video/201802/tdy_pop_klg_steak_180215_1920x1080.today-inline-vid-featured-desktop.jpg'
     )
   ];
 
